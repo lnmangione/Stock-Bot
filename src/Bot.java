cb7c7d584ecd226b27db6c92759206f784a3b4e4
Bryce <phdbaj@yahoo.com>
2015-07-21 14:35:07 -0700

    Initial commit

---
+++ src/Bot.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bot {
+	static final String ds = "$";
+	
+	public static void main(String[] args) {
+		Symbol symbol;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Stock: ");
+		String stock = sc.next();
+		symbol = new Symbol(stock);
+		
+		System.out.println("Symbol: " + symbol.getSymbol());
+		System.out.println("Price: " + ds + symbol.getPrice());
+		System.out.println("Shares: " + symbol.getNumberOfShares());
+	}
+}
---
+++ src/Symbol.java
@@ -0,0 +1,73 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.histquotes.Interval;
+
+public class Symbol {
+	String symbol;
+	BigDecimal price, change, dayHigh, dayLow, ftWkHigh, ftWkLow, eps;
+	long volume, shares;
+	
+	public Symbol(String stock){
+		setInfo(stock);
+	}
+	 
+	//Returns History
+		public List<HistoricalQuote> getHistory(int months){
+		Stock t = YahooFinance.get(symbol);
+		Calendar from = Calendar.getInstance();
+		Calendar to = Calendar.getInstance();
+		from.add(Calendar.MONTH, - months); //4 Months Ago
+		List<HistoricalQuote> quotes = t.getHistory(from, to, Interval.DAILY);
+		return quotes;
+	}
+		
+	//Sets Features
+		private void setInfo(String ticker){
+		Stock stock = YahooFinance.get(ticker);
+		symbol = ticker;
+		price = stock.getQuote().getPrice();
+		change = stock.getQuote().getChangeInPercent();
+		volume =stock.getQuote().getVolume();  
+		dayHigh = stock.getQuote().getDayHigh(); 
+	 	dayLow = stock.getQuote().getDayLow(); 
+	 	ftWkHigh = stock.getQuote().getYearHigh(); 
+	 	ftWkLow = stock.getQuote().getYearLow(); 
+	 	eps = stock.getStats().getEps();
+	 	shares = stock.getStats().getSharesOutstanding();
+	 	BigDecimal twoHundredChange = stock.getQuote().getChangeFromAvg200();
+	} 
+	
+	//Returns Features
+		public String getSymbol(){
+			return symbol;
+		}
+		public BigDecimal getDayHigh(){
+			return dayHigh;
+		}
+		public BigDecimal getDayLow(){
+			return dayLow;
+		}
+		public BigDecimal getFtWkHigh(){
+			return ftWkHigh;
+		}
+		public BigDecimal getFtWkLow(){
+			return ftWkLow;
+		}
+		public BigDecimal getPrice(){
+			return price;
+		}
+		public BigDecimal getEPS(){
+			return eps;
+		}
+		public long getNumberOfShares(){
+			return shares;
+		}
+		public long getVolume(){
+			return volume;
+		}
+}
