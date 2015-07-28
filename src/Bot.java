96c684a7f2334263d3b9ad439a234028cb5f3989
Michael Bick <MichaelBick@Michaels-MacBook-Pro-5.local>
2015-07-28 02:44:23 -0700

    Merged testing code back in

--- src/Bot.java
+++ src/Bot.java
@@ -9,19 +9,31 @@ public class Bot {
 		Scanner sc = new Scanner(System.in);
 		
 		/*
-		// Get a stock sybol to check
+		// Get a stock symbol to check
 		System.out.print("Stock: ");
 		String stock = sc.next();
 		symbol = new Symbol(stock);
 		*/
-		
-		symbol = new Symbol("AAPL");
-		
+
+		symbol = new Symbol("^GSPC");
+
+		/*
 		System.out.println("Symbol: " + symbol.getSymbol());
 		System.out.println("Price: " + ds + symbol.getPrice());
 		// System.out.println("Shares: " + symbol.getNumberOfShares());
 		System.out.println("Moving Average: " + symbol.getMA(0, 200));
 		System.out.println("Moving Average: " + symbol.getMA(0, 50));
+		*/
+
+		Symbol[] trainingSet = new Symbol[5];
+		trainingSet[0] = new Symbol("^GSPC");
+		trainingSet[1] = new Symbol("GOOGL");
+		trainingSet[2] = new Symbol("ACE");
+		trainingSet[3] = new Symbol("HD");
+		trainingSet[4] = new Symbol("HP");
+
+		double[] weightsArray = RegressionAlgorithm.linearLearner(trainingSet, 7, 0.1);
+		RegressionAlgorithm.predictY(new Symbol("AAPL"), 10, weightsArray);
 		
 		sc.close();
 	}
