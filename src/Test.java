d45faa21646eb657ed76f0639f97b23a34291c33
Michael Bick <mafbick@gmail.com>
2015-07-29 12:30:02 -0700

    Made test() method non-static

--- src/Test.java
+++ src/Test.java
@@ -4,22 +4,31 @@ import java.io.IOException;
  * Created by MichaelBick on 7/29/15.
  */
 public class Test {
+	private Symbol[] trainStocks;
+	private Symbol[] testStocks;
+	
     public static void main(String[] args) throws IOException {
-        test();
+    	Symbol[] stocks = {new Symbol("AAPL")};
+    	
+    	Test test = new Test(stocks, stocks);
+    	test.test();
+    }
+    
+    public Test (Symbol[] trainStocks, Symbol[] testStocks) {
+    	this.trainStocks = trainStocks;
+    	this.testStocks = testStocks;
     }
 
-    private static void test() throws IOException {
-        Symbol[] stocks = {new Symbol("AAPL")};
-
+    private void test() throws IOException {
         int FUTURE_DAYS = 10;
         int NUM_POINTS = 20;
         int DAYS_BACK = 100;
 
-        double[][] train = GradientDescent.getData(stocks, NUM_POINTS, DAYS_BACK);
-        double[] trainActual = GradientDescent.getActual(stocks, NUM_POINTS, DAYS_BACK, FUTURE_DAYS);
+        double[][] train = GradientDescent.getData(trainStocks, NUM_POINTS, DAYS_BACK);
+        double[] trainActual = GradientDescent.getActual(trainStocks, NUM_POINTS, DAYS_BACK, FUTURE_DAYS);
 
-        double[][] test = GradientDescent.getData(stocks, NUM_POINTS, DAYS_BACK + NUM_POINTS);
-        double[] testActual = GradientDescent.getActual(stocks, NUM_POINTS, DAYS_BACK + NUM_POINTS, FUTURE_DAYS);
+        double[][] test = GradientDescent.getData(testStocks, NUM_POINTS, DAYS_BACK + NUM_POINTS);
+        double[] testActual = GradientDescent.getActual(testStocks, NUM_POINTS, DAYS_BACK + NUM_POINTS, FUTURE_DAYS);
 
         double[] theta = new double[train[0].length];
 
