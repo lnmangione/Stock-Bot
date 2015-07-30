f4cabfed3db26a699fa97b2ab28cfe22fc93ecde
Michael Bick <MichaelBick@Michaels-MacBook-Pro-5.local>
2015-07-30 00:23:27 -0700

    Cleaned up old code

--- src/Bot.java
+++ src/Bot.java
@@ -8,44 +8,6 @@ public class Bot {
 	static final String ds = "$";
 	
 	public static void main(String[] args) throws IOException {
-		/*
-		Symbol symbol;
-		Scanner sc = new Scanner(System.in);
-		
-		
-		// Get a stock symbol to check
-		System.out.print("Stock: ");
-		String stock = sc.next();
-		symbol = new Symbol(stock);
-		
-
-		symbol = new Symbol("AAPL");
-
-
-		System.out.println("Symbol: " + symbol.getSymbol());
-		System.out.println("Price: " + ds + symbol.getPrice());
-		// System.out.println("Shares: " + symbol.getNumberOfShares());
-		System.out.println("Moving Average: " + symbol.getMA(0, 200));
-		System.out.println("Moving Average: " + symbol.getMA(0, 50));
-		// System.out.println("Closing Price: " + symbol.getAdjClose(30));
-		*/
-
-		
-		//Symbol[] trainingSet = getRandomStocks(5);
-		Symbol[] trainingSet = new Symbol[5];
-		trainingSet[0] = new Symbol("HD");
-		trainingSet[1] = new Symbol("HP");
-		trainingSet[2] = new Symbol("ABC");
-		trainingSet[3] = new Symbol("GOOGL");
-		trainingSet[4] = new Symbol("MMM");
-		
-		double[] weightsArray = RegressionAlgorithm.linearLearner(trainingSet, 10, 0.000001);
-		
-		Symbol appleSymbol = new Symbol("AAPL");
-		double prediction = RegressionAlgorithm.predictY(appleSymbol, 10, weightsArray);
-		System.out.print("Actual: " + appleSymbol.getPrice().doubleValue() + ", Predicted: " + prediction);
-
-		//sc.close();
 	}
 	
 	private static Symbol[] getRandomStocks(int num) throws IOException {
