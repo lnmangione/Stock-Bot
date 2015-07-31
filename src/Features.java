719065cd06f83418cb004e9d99393ca6e255a533
Michael Bick <mafbick@gmail.com>
2015-07-30 22:49:58 -0700

    Added features

--- src/Features.java
+++ src/Features.java
@@ -17,28 +17,18 @@ public class Features {
         sp500 = new Symbol("^GSPC");
     }
 
-    private double getSP500(int daysAgo) throws IOException{
-        BigDecimal price = market.get(daysAgo);
-
-        if (price == null) {
-            price = sp500.getAdjClose(daysAgo);
-            market.put(daysAgo, price);
-        }
-
-        return price.doubleValue();
-    }
-
     public double[] getFeatures(Symbol stock, int daysAgo) throws IOException {
-        double[] features = new double[5];
+        double[] features = new double[6];
 
         features[0] = 1.0;
 
         features[1] = stock.getAdjClose(daysAgo).doubleValue();
+        features[2] = sp500.getAdjClose(daysAgo).doubleValue();
         
-        features[features.length - 3] = stock.getMA(daysAgo, 50).doubleValue() - stock.getMA(daysAgo, 200).doubleValue();
+        features[features.length - 3] = stock.getMA(daysAgo, 50).doubleValue();
+        features[features.length - 2] = stock.getMA(daysAgo, 200).doubleValue();
         
-        features[features.length - 2] = stock.getMA(daysAgo, 50).doubleValue();
-        features[features.length - 1] = stock.getMA(daysAgo, 200).doubleValue();
+        features[features.length - 1] = sp500.getMA(daysAgo, 50).doubleValue();
 
         return features;
     }
